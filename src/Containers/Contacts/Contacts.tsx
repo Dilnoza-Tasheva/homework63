99690d8920b8fd8936e336abc292d0030c52203a
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-25 20:23:31 +0200

    Populated Contacts container with basic MUI components

--- src/Containers/Contacts/Contacts.tsx
+++ src/Containers/Contacts/Contacts.tsx
@@ -1,10 +1,28 @@
+import { Box, Container, Typography } from '@mui/material';
 
 
 const Contacts = () => {
   return (
-    <div>
-      Contacts
-    </div>
+    <Container maxWidth="md">
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h5" gutterBottom>
+          Contact me
+        </Typography>
+        <Typography variant="body1">
+          You can reach me through the following methods:
+        </Typography>
+        <hr/>
+        <Typography variant="body1">
+          <strong>Email:</strong> example@email.com
+        </Typography>
+        <Typography variant="body1">
+          <strong>Phone:</strong> +1 123 456-789
+        </Typography>
+        <Typography variant="body1">
+          <strong>Address:</strong> 123 street, BL 12345
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 

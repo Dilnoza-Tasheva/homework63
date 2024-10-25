83d8d45983797de5f80b5d00533fff964eb8d695
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-25 20:17:10 +0200

    Populated About container with basic MUI components

--- src/Containers/About/About.tsx
+++ src/Containers/About/About.tsx
@@ -1,10 +1,18 @@
+import { Box, Container, Typography } from '@mui/material';
 
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <Container maxWidth="md">
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h5" gutterBottom>
+          About my blog
+        </Typography>
+        <Typography variant="body1">
+          Welcome to my blog application! Here you can read and share your thoughts.
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
